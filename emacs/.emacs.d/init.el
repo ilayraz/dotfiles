4100f7ad77393211505e0280585cb08c106b7714
Ilay Raz <ilay.raz@gmail.com>
2018-06-09 11:05:08 -0700

    Update emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,7 +6,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/College/MATH23A_Written_HW_1.org")))
  '(package-selected-packages
    (quote
     (helm-bibtexkey interleave htmlize org-bullets direx elpy auto-complete magit moe-theme powerline diff-hl))))
